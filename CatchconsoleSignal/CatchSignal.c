afa9ebbdcf9b6ba8376f52c161f6b8a099746a9b
NaughtyCode <1774395067@qq.com>
2014-02-03 17:26:45 +0800

    Catch console Signal.

---
+++ CatchconsoleSignal/CatchSignal.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defiend(WIN32)
+#include <windows.h>
+BOOL WINAPI SignalHandler(DWORD dwCtrlType)
+{
+	if(dwCtrlType == CTRL_C_EVENT)
+	{
+		printf("Ctrl + C has been pressed.\n");
+	}else
+	{
+		exit(0);
+	}
+}
+
+void SetSignalCallBack(){
+	SetConsoleCtrlHandler(SignalHandler, TRUE);
+}
+
+#else
+#include <signal.h>
+
+void SignalHandler(int s){
+	printf("Caught signal %d\n",s);
+	exit(1); 
+
+}
+
+void SetSignalCallBack(){
+	static struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = SignalHandler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+}
+
+#endif
+
+
+// test
+int main(int argc, char *argv[])
+{
+	SetSignalCallBack();
+	printf("Try to press Ctrl + C\n");
+	Sleep(-1);
+	return 0;
+}
